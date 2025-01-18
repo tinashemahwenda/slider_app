138e9a497e3e55fef719508aaa331884ed23cf04
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-01-18 15:31:01 +0200

    Removed that nonsense slider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:sleek_circular_slider/sleek_circular_slider.dart';
 
 void main() {
   runApp(MoodSlider());
